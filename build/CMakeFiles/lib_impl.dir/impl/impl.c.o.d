d01f8f256f752aa2da4032cfd53b8a5a907b4970
UB <avinash.aksinha@outlook.com>
2025-06-22 08:55:36 +0000

    More Personal

--- build/CMakeFiles/lib_impl.dir/impl/impl.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lib_impl.dir/impl/impl.c.o: \
- /mnt/c/Users/Asus/Favorites/basee/base/impl/impl.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/malloc_l.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for malloc_l.
-# This may be replaced when dependencies are built.
